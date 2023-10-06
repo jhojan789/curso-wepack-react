bfa60ee8958600fa3168e4d9c1a9d9f8a602d53a
Jhojan Tobón <jhojantobonm@gmail.com>
2023-10-06 16:59:24 -0500

    updated the react syntax and added import to App commponent

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from "react-dom/client";
 import App from './components/App.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+
+
+
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App/>);
